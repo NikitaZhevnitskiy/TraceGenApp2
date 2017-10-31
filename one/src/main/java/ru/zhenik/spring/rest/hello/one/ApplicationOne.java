d5a832b550b5b3acba7882d5e292b11bb9774d7a
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-10-31 21:15:05 +0100

    add module one with chaining and tracer

--- one/src/main/java/ru/zhenik/spring/rest/hello/one/ApplicationOne.java
+++ one/src/main/java/ru/zhenik/spring/rest/hello/one/ApplicationOne.java
@@ -1,21 +1,15 @@
-package ru.zhenik.spring.rest.hello;
+package ru.zhenik.spring.rest.hello.one;
 
+import com.uber.jaeger.Configuration;
+import com.uber.jaeger.samplers.ProbabilisticSampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import com.uber.jaeger.Configuration;
-import com.uber.jaeger.samplers.ProbabilisticSampler;
 import org.springframework.web.client.RestTemplate;
 
-
-/**
- * hello
- * NIK on 09/10/2017
- */
 @SpringBootApplication
-public class Application {
-
+public class ApplicationOne {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
@@ -23,13 +17,13 @@ public class Application {
     }
 
     @Bean
-    public io.opentracing.Tracer tracer() {
-        return new Configuration("spring-boot", new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
+    public io.opentracing.Tracer jaegerTracer() {
+        return new Configuration("one-module", new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
                 new Configuration.ReporterConfiguration())
                 .getTracer();
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ApplicationOne.class, args);
     }
 }
---
+++ one/src/main/java/ru/zhenik/spring/rest/hello/one/controller/GreetingController.java
@@ -0,0 +1,26 @@
+package ru.zhenik.spring.rest.hello.one.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class GreetingController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/inside")
+    public String chaining() {
+        return "inside";
+    }
+
+    @RequestMapping("/outside")
+    public String outside() {
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8082/me", String.class);
+        return "Chaining + " + response.getBody();
+    }
+}
